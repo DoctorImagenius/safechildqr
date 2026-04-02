b1fe9a845f19c76ae90b26fd327c0ce3728b5c4c
haroonbabar <haroon.babar@revoech.com>
2026-04-02 22:45:50 +0000

    update mailWorker file

--- jobs/mailWorker.js
+++ jobs/mailWorker.js
@@ -7,17 +7,17 @@ mailQueue.process("sendEmail", async (job) => {
     const { email, childName, ip, deviceInfo } = job.data;
     console.log(`📧 Sending email to ${email}...`);
 
-    // const time = new Date().toUTCString();
-    // const location = await getLocation(ip);
+    const time = new Date().toUTCString();
+    const location = await getLocation(ip);
 
-    // const html = generateEmailHTML({ childName, ip, time, location, deviceInfo });
+    const html = generateEmailHTML({ childName, ip, time, location, deviceInfo });
 
-    // await transporter.sendMail({
-    //     from: `"SafeChildQR 🚨" <${process.env.EMAIL_USER}>`,
-    //     to: email,
-    //     subject: "🚨 SafeChildQR Alert",
-    //     html,
-    // });
+    await transporter.sendMail({
+        from: `"SafeChildQR 🚨" <${process.env.EMAIL_USER}>`,
+        to: email,
+        subject: "🚨 SafeChildQR Alert",
+        html,
+    });
 });
 
 mailQueue.on("completed", (job) => {
